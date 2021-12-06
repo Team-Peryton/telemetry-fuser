cd3b6c0ed0965defda5e72c5538e6c780921d61e
Latobius <artiejvk@hotmail.co.uk>
2021-12-06 14:04:23 +0100

    Basic setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,69 @@
-// formatted with 4 spaces as indent
-
 #include <Arduino.h>
+
+/*  Serial aliases  */
+#define RADIO   Serial1 
+#define PILOT   Serial2
+#define VISOR   Serial3
+
 
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
+    
+    RADIO.begin(57600);       // initialise Serial1 : Connection to radio
+    PILOT.begin(57600);       // initialise Serial2 : Connection to Ardupilot / Mission Planner 
+    VISOR.begin(57600);       // initialise Serial3 : Connection to Autovisor / Co-computer
+
+    /*  Setup SD card logging here */
+
 }
 
 void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(200);                       // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    delay(200);                       // wait for a second
+    // digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    // digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+
+
+    /*  Core forwarding loop: 
+    Loop
+
+        Do While "packet from PILOT" OR "packet from RADIO marked as PILOT" 
+            If packet from PILOT 
+                Forward to RADIO 
+            If packet from RADIO marked as PILOT
+                Forward to PILOT
+        Loop
+
+        If packet from VISOR
+            Forward to RADIO
+        If packet from RADIO marked as VISOR
+            Forward to VISOR
+
+    While True
+    */
+}
+
+void radioIn() {
+
+}
+
+void radioOut() {
+
+    RADIO.flush();
+}
+
+void pilotIn() {
+
+}
+
+void pilotOut() {
+
+    PILOT.flush();
+}
+
+void visorIn() {
+
+}
+
+void visorOut() {
+
+    VISOR.flush();
 }
