90032ef51d2f22b31405a193325a180756e0afbf
Latobius <artiejvk@hotmail.co.uk>
2021-12-15 16:29:39 +0000

    v0.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,16 @@
 #include <Arduino.h>
 
-// SD card libraries
+/* SD card libraries */
 #include <SPI.h>
 #include <SD.h>
 
-/*  Serial aliases  */
+/* Serial aliases */
 #define RADIO Serial1
 #define PILOT Serial2
 #define VISOR Serial3
 
-unsigned long id;
+/* Define max message length to avoid using dynamic memory. (The MAVLink max packet size is 280 bytes) */
+#define MAX_LENGTH 320
 
 void setup()
 {
@@ -41,27 +42,27 @@ void setup()
         digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
         delay(600);
     }
-
-    id = 0; // Resets packet id counter
-}
-
-/* Sends an addressed packet via the RADIO, returning whether the correct number of bytes were written (WIP) */
-bool sendLetter(String message)
-{
-    RADIO.println(message);
 }
 
 /*  Takes a packet, addresses it to the PILOT or the VISOR, formats it for radio transmission, then returns the formatted message  */
-String addressTo(String packet, String recipient, unsigned long messageId)
+void addressTo(char packet[MAX_LENGTH], char recipient[5], unsigned long int messageId)
 {
-    // return format {id:1,recipient:VISOR,message:Crash immediately} (should in theory automatically be appended with null character '\0')
-    return String("{id:" + String(messageId) + ",recipient:" + recipient + ",message:" + packet + "}");
+    // return format {id:1,recipient:VISOR,message:Crash immediately}
+    // packet = ("{id:" + messageId + ",recipient:" + recipient + ",message:" + packet + "}");
+    packet = strcat(packet,recipient);
 }
+
+/* Sends a packet via the RADIO, returning whether the correct number of bytes were written (WIP) */
+bool sendLetter(char packet[MAX_LENGTH], char recipient[5], unsigned long int messageID)
+{
+    addressTo(packet, recipient, messageID);
+    RADIO.println(packet);
+}
+
 
 /*  Opens a message from the RADIO, returns the intended recipient  */
 String readAddress(String packet)
 {
-    
 }
 
 /*  Opens a message from the RADIO, returns the id of the message  */
@@ -74,44 +75,19 @@ String openLetter(String packet)
 {
 }
 
-void radioIn()
-{
-}
-
-void radioOut()
-{
-
-    RADIO.flush();
-}
-
-void pilotIn()
-{
-}
-
-void pilotOut()
-{
-
-    PILOT.flush();
-}
-
-void visorIn()
-{
-}
-
-void visorOut()
-{
-
-    VISOR.flush();
-}
 
 // loop() goes at the end to avoid compile errors
 void loop()
 {
-    String sReceivedPacket;
-    String sForwardPacket;
 
-    sReceivedPacket.reserve(300); // reserving memory for strings increases speed
-    sForwardPacket.reserve(300);
+    /* The idea of the ID is that if either end receives a mesage with an ID which doesn't match their counter,
+    they can deduce that message(s) have been missed, and they can then resynchronise. 
+    Static should allow it to retain its value between loops */
+    static unsigned long int id = 0; // Message ID counter
+
+    static char currentmessage[MAX_LENGTH]; // Message variable to be pointed at by functions
+    static char recipient[5]; // Recipient: either "PILOT" or "VISOR"
+
 
     /*  Core forwarding loop: 
 
@@ -126,12 +102,16 @@ void loop()
         Forward to RADIO
     If packet from RADIO marked as VISOR
         Forward to VISOR
-
     */
 
-    char sPayloadExample[] = "This is a test message which is just longer than 64 bytes, for science";
+    /* on receiving letter: 
+    if letter ID < current ID, packets were missed at the other end
+    if letter ID = current ID, no packets were missed, or an equal number of packets were missed
+    if letter ID > current ID, packets were missed at this end */
 
-    sReceivedPacket = sPayloadExample;
 
-    sForwardPacket = addressTo(sReceivedPacket, "VISOR", id);
+    // sendLetter();
+    sendLetter(currentmessage, recipient, id);
+    id++; // Increments ID after sending letter
+
 }
