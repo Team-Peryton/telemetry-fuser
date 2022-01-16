c0cdfc6a467d4d4b8a6974dcd25084f2f93302e7
Latobius <artiejvk@hotmail.co.uk>
2022-01-16 14:24:37 +0100

    more

--- TODO.md
+++ TODO.md
@@ -2,13 +2,16 @@
 
 ## Critical
 
-- Write code that does things
+- Actually Read and Write to Serial ports
+- `Serial.print()` or `Serial.write()`
 
 ## Important
 
-- Migrate from `String` to `char` to improve performance
-- Explore Serial.availableForWrite() 
+- Explore buffer size
 - Check which pin the Teensy uses for the SD card
+- Test every single function
+- Maybe redesign packets so that `peek()` can be used to identify the destination
 
 ## Useful
-- 
+
+- Migrate from `String` to `char` to improve performance
--- src/main.cpp
+++ src/main.cpp
@@ -11,21 +11,25 @@
 
 /* Define max message length to avoid using dynamic memory. (The MAVLink max packet size is 280 bytes) */
 // #define MAX_LENGTH 320
-// Only needed if we work with Chars
+// Only needed if working with Chars
+
+void led(int b)
+{
+    digitalWrite(LED_BUILTIN, b);
+}
 
 void setup()
 {
     pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, LOW);
 
     /* SD card begin */
     if (!SD.begin())
     { /*  ==Fast blink 10 times if the SD card fails to initialise==   */
         for (int i = 0; i <= 10; i++)
         {
-            digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+            led(HIGH); // turn the LED on (HIGH is the voltage level)
             delay(200);
-            digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
+            led(LOW); // turn the LED off by making the voltage LOW
             delay(100);
         }
     }
@@ -34,21 +38,20 @@ void setup()
     RADIO.begin(57600); // initialise Serial1 : Connection to radio
     PILOT.begin(57600); // initialise Serial2 : Connection to Ardupilot / Mission Planner
     VISOR.begin(57600); // initialise Serial3 : Connection to Autovisor / Co-computer
-
-    while (!RADIO || !PILOT || !VISOR)
-    {
-        /* Slow flash while waiting for serial ports to initialise */
-        digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
-        delay(600);
-        digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
-        delay(600);
-    }
 }
 
-/* Sends an addressed packet via the RADIO, returning whether the correct number of bytes were written (WIP) */
+/* Sends an addressed packet via the RADIO, returning whether the correct number of bytes were written */
 bool sendLetter(String message)
 {
-    RADIO.println(message);
+    byte bytesWritten = RADIO.println(message);
+    if (bytesWritten == message.length())
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 /*  Takes a packet, addresses it to the PILOT or the VISOR, formats it for radio transmission, then returns the formatted message  */
@@ -61,79 +64,98 @@ String addressTo(String packet, String recipient, unsigned long messageId)
 /*  Opens a message from the RADIO, returns the intended recipient  */
 String readAddress(String packet)
 {
-    String search = "recipient:";  // tag to search for
-    byte index = packet.indexOf(search) + search.length();    // start index of recipient
-    return packet.substring(index, index + 5);    // + 5 because that is the length of the 3 possible recipients: RADIO, PILOT, VISOR
+    String search = "recipient:";                          // tag to search for
+    byte index = packet.indexOf(search) + search.length(); // start index of recipient
+    return packet.substring(index, index + 5);             // + 5 because that is the length of the 3 possible recipients: RADIO, PILOT, VISOR
 }
 
 /*  Opens a message from the RADIO, returns the id of the message  */
 unsigned long readID(String packet)
 {
-    String search = "id:";  // tag to search for
-    byte startIndex = packet.indexOf(search) + search.length();    // start index of tag
-    byte endIndex = packet.indexOf(",");    // index of first "," delimiter
-    return packet.substring(startIndex, endIndex).toInt();    // the internet told me that .toInt() should work even though it's unsigned long
+    String search = "id:";                                      // tag to search for
+    byte startIndex = packet.indexOf(search) + search.length(); // start index of tag
+    byte endIndex = packet.indexOf(",");                        // index of first "," delimiter
+    return packet.substring(startIndex, endIndex).toInt();      // the internet told me that .toInt() should work even though it's unsigned long
 }
 
 /*  Opens a message from the RADIO, returns the packet contained inside  */
-String openLetter(String packet)
+String readMessage(String packet)
 {
-    String search = "message:";  // tag to search for
-    byte startIndex = packet.indexOf(search) + search.length();    // start index of tag
-    byte endIndex = packet.lastIndexOf("}");    // index of last "}" tag. Should work even if message contains "}" and if the packet  is appended with null characters
-    return packet.substring(startIndex, endIndex).toInt();    // the internet told me that .toInt() should work even though it's unsigned long
+    String search = "message:";                                 // tag to search for
+    byte startIndex = packet.indexOf(search) + search.length(); // start index of tag
+    byte endIndex = packet.lastIndexOf("}");                    // index of last "}" tag. Should work even if message contains "}" and if the packet is appended with null characters
+    return packet.substring(startIndex, endIndex);
 }
 
-void radioIn()
+void radioSort()
 {
 }
-
-void radioOut()
-{
-
-    RADIO.flush();
-}
-
-void pilotIn()
+void pilotSort()
 {
 }
-
-void pilotOut()
+void visorSort()
 {
-
-    PILOT.flush();
-}
-
-void visorIn()
-{
-}
-
-void visorOut()
-{
-
-    VISOR.flush();
 }
 
 // loop() goes at the end to avoid compile errors
 void loop()
 {
-    // [Turn on LED when there are packets in the buffer]
-    // [Turn off LED when the packet buffer has been emptied]
 
-    
-        /* The idea of the ID is that if either end receives a mesage with an ID which doesn't match their counter,
+    while (!RADIO || !PILOT || !VISOR)
+    {
+        /* Slow flash while waiting for serial ports to initialise */
+        led(HIGH); // turn the LED on (HIGH is the voltage level)
+        delay(600);
+        led(LOW); // turn the LED off by making the voltage LOW
+        delay(600);
+    }
+
+    /* The idea of the ID is that if either end receives a mesage with an ID which doesn't match their counter,
     they can deduce that message(s) have been missed, and they can then resynchronise. 
     Static should allow it to retain its value between loops */
     static unsigned long int id = 0; // Message ID counter
 
+    bool priority = false; // Does the Autopilot take priority over the co-computer for this loop?
 
     String sReceivedPacket;
     String sForwardPacket;
 
-    sReceivedPacket.reserve(300); // reserving memory for strings increases speed
+    sReceivedPacket.reserve(300); // reserving memory for strings supposedly increases speed
     sForwardPacket.reserve(300);
 
-    /*  Core forwarding loop: 
+    // Turn on LED when there are packets in the buffer
+    // Turn off LED when the packet buffer has been emptied
+
+    // Forwards from Autopilot / Mission Planner
+    if (PILOT.available() > 0)
+    {
+        led(HIGH);
+        priority = true;
+
+        pilotSort();
+    }
+
+    if (RADIO.available() > 0)
+    {
+        led(HIGH);
+        priority = true;
+
+        radioSort();
+    }
+
+    // Forwards to/from the Autovisor, but only if there were no packets to/from the Autopilot
+    if (priority == false && VISOR.available() > 0)
+    {
+        led(HIGH);
+
+        visorSort();
+    }
+    if (priority == false && VISOR.available() == 0)
+    {
+        led(LOW);
+    }
+
+    /*  Core forwarding loop ultimate goal (not quite there yet): 
 
     Do While "packet from PILOT" OR "packet from RADIO marked as PILOT" 
         If packet from PILOT 
@@ -148,10 +170,4 @@ void loop()
         Forward to VISOR
 
     */
-
-    char sPayloadExample[] = "This is a test message which is just longer than 64 bytes, for science";
-
-    sReceivedPacket = sPayloadExample;
-
-    sForwardPacket = addressTo(sReceivedPacket, "VISOR", id);
 }
